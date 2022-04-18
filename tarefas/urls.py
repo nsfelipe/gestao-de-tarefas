4fb71c67ca9d9dd220597e2b5d19b3954ffec6d9
Felipe Nunes <nunesfelipe5@gmail.com>
2022-04-18 11:02:11 -0300

    Versão final do projeto

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('<int:tarefa_id>/excluir', views.excluir_tarefa, name='excluir_tarefa'),
     path('<int:tarefa_id>/adiar', views.adiar_tarefa, name='adiar_tarefa'),
     path('<int:tarefa_id>/editar', views.editar_tarefa, name='editar_tarefa'),
-    path('concluidas/', views.tarefas_concluidas_list, name='tarefas_concluidas_list')
+    path('concluidas/', views.tarefas_concluidas_list, name='tarefas_concluidas_list'),
+    path('adiadas/', views.tarefas_adiadas_list, name='tarefas_adiadas_list'),
+    path('<int:tarefa_id>/mover-para-lista-de-tarefas/', views.mover_para_tarefas, name='mover_para_tarefas'),
+    path('adicionar/', views.adicionar_tarefas, name='adicionar_tarefas')
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -36,6 +36,7 @@ def adiar_tarefa(request, tarefa_id):
     tarefa.save()
     return redirect('tarefas_pendentes_list')
 
+# Função de editar a tarefa
 def editar_tarefa(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, id=tarefa_id)
     if request.method == 'POST':
@@ -50,6 +51,30 @@ def editar_tarefa(request, tarefa_id):
         form = EditarTarefaForm(initial={'tarefa':tarefa.descricao, 'categoria':tarefa.categoria})
     return render(request, 'tarefas/editar_tarefa.html', {'tarefa':tarefa, 'form':form} )
 
+# Função para exibir a tarefas concluídas
 def tarefas_concluidas_list(request):
     tarefas_concluidas = Tarefa.objects.filter(status='concluído')
     return render(request, 'tarefas/tarefas_concluidas.html', {'tarefas_concluidas':tarefas_concluidas})
+
+# Função para exibir a tarefas adiadas
+def tarefas_adiadas_list(request):
+    tarefas_adiadas = Tarefa.objects.filter(status='adiado')
+    return render(request, 'tarefas/tarefas_adiadas.html', {'tarefas_adiadas':tarefas_adiadas})
+
+# Função para mover tarefas de adiadas para lista de tarefas
+def mover_para_tarefas(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    tarefa.status = 'pendente'
+    tarefa.save()
+    return redirect('tarefas_pendentes_list')
+
+# Função da pagina adicionar_tarefas
+def adicionar_tarefas(request):
+    if request.method == 'POST':
+        form = AdicionarTarefa(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('adicionar_tarefas')
+    else:
+        form = AdicionarTarefa()
+    return render(request, 'tarefas/adicionar_tarefas.html', {'adicionar_tarefas':adicionar_tarefas, 'form':form})
